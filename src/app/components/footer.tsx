7324ee767562ead750f0213ba6c26849f92b81ff
Aron Vinicius <avinicius@unicargo.com.br>
2026-03-16 13:10:49 -0300

    feat: update year

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,10 +1,11 @@
 import styles from "../styles/Footer.module.css";
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <p className={styles.footerText}>
-        Copyright © Unicargo 2025. Todos os direitos reservados.
+        Copyright © Unicargo 2025 - {currentYear}. Todos os direitos reservados.
       </p>
     </footer>
   );
