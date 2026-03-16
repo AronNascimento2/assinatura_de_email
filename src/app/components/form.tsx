57bcb6368ddb17db21ab89f739a6f918e8d83095
Aron Vinicius <avinicius@unicargo.com.br>
2026-03-16 12:28:53 -0300

    feat: update 3

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -92,7 +92,7 @@ export const Form: React.FC<FormProps> = ({
           className={styles.input}
           type="text"
           id="ramal"
-          placeholder="Digite o ramal "
+          placeholder="Digite o ramal"
           pattern="\d{4}"
           required
         />
